8feea08b6f5fe414241ab69c377c632b3441fd1e
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-11-21 01:44:26 -0300

    App Tracker

--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import static
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtailautocomplete.urls.admin import urlpatterns as autocomplete_admin_urls
 
 from core.search import views as search_views
 
 urlpatterns = [
+    path("admin/autocomplete/", include(autocomplete_admin_urls)),
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
